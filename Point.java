36b352669cffd6e7a43a18e26015fac218f7c369
135031 <plevnaroski.kliment@students.finki.ukim.mk>
2020-04-12 22:17:01 +0200

    'point class finished

--- Point.java
+++ Point.java
@@ -1,20 +1,71 @@
 class Point {
-	String id, pointColor;
+	String id, pColor;
 	double x,y;
 
-	//TODO add new variable
+	public Point(String id, double x, double y, String pointsColor) {
+		this.id = id;
+		this.x = x;
+		this.y = y;
+		this.pointsColor = pointsColor;
+	}
 
-	//TODO constructor
 
-	//TODO setters and getters
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public void setPcolor(String pColor) {
+		this.pColor = pColor;
+	}
+
+	public void setX(double x) {
+		this.x = x;
+	}
+
+	public void setY(double y) {
+		this.y = y;
+	}
+
+	public String getId() {
+		return id;
+	}
+
+	public String getPcolor() {
+		return pointsColor;
+	}
+
+	public double getX() {
+		return x;
+	}
+
+	public double getY() {
+		return y;
+	}
+
 
 	public void move (char xDirection, char yDirection) {
-		//TODO
+		if(xDirection=='L'){
+            x--;
+        }
+		else if(xDirection=='R'){
+            x++;
+        }
+
+        if(yDirection=='U'){
+            y++;
+        }
+		else if(yDirection=='D'){
+            y--;
+        }
 	}
 
 	public void draw () {
-		//TODO
+		System.out.println(String.format("Point coordnates: x = %f y = %f", x, y));
 	}
+
+
+
+
 
 
 
